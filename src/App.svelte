60d67986ca8d4ab26aeabac4c2a5967dfe0c5337
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-04-25 18:42:44 -0300

    create controls

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,51 @@
 <script>
-	export let name;
+	let name = "Lincoln";
+  let points = 100;
+  let showControls = false;
+  
+  const addPoint = () => points +=1;
+  const removePoint = () => points -=1;
+  const toggleControls= () => showControls = !showControls;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+  <div class="card">
+    <h1>
+      {name}
+      <button class="btn btn-sm" on:click={toggleControls}>
+        {#if showControls }-{:else}+{/if}
+      </button>
+    </h1>
+    <h3>Points: {points}</h3>
+    {#if showControls }
+      <button class="btn" on:click={addPoint}>+1</button>
+      <button class="btn btn-dark" on:click={removePoint}>-1</button>
+      <input type="number" name="" id="" bind:value={points}>
+    {/if}
+  </div>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 500px;
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    font-weight: 100;
+  }
+
+  .btn-sm {
+    font-size: 0.5em;
+  }
+
+  .card{
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
 
 	@media (min-width: 640px) {
 		main {
